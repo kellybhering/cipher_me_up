72c3d4bcd08e1c97ffc4b77b99896a3d5fe84d4c
Kelly Bhering <kellybhering@hotmail.com>
2021-09-23 14:45:24 -0300

    Enables ciphering words with unpermitted characters by ignoring them and corrects tests of FixedShiftCipher

---
+++ spec/strategies/fixed_shift_cipher_spec.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe FixedShiftCipher do
+  describe '.cipher' do
+    context 'with an informed shift factor' do
+    end
+
+    context 'with default shift factor' do
+      subject { FixedShiftCipher.cipher(word) }
+
+      context 'and a word with letters and numbers' do
+        let(:word) { 'HelloWorld123' }
+  
+        it 'should return the ciphered word correctly' do
+          is_expected.to eq('KhoorZruog456')
+        end
+      end
+
+      context 'and a letter in which the ciphering would exceed z' do
+        let(:word) { 'HxlloWorld123' }
+  
+        it 'should restart the alphabet and return the ciphered word correctly' do
+          is_expected.to eq('KaoorZruog456')
+        end
+      end
+  
+      context 'and a number in which the ciphering would exceed 9' do
+        let(:word) { 'HelloWorld128' }
+  
+        it 'should return the ciphered word correctly' do
+          is_expected.to eq('KhoorZruog451')
+        end
+      end
+
+      context 'and a word with any other character than letters and numbers' do
+        let(:word) { 'HelloWorld129!' }
+  
+        it 'should ignore unpermitted characters and return the ciphered word correctly' do
+          is_expected.to eq('KhoorZruog452!')
+        end
+      end
+    end   
+  end
+
+  describe 'uncipher' do
+    context 'with default shift factor' do
+      subject { FixedShiftCipher.uncipher(word) }
+
+      context 'and a word with letters and numbers' do
+        let(:word) { 'KhoorZruog456' }
+  
+        it 'should return the unciphered word correctly' do
+          is_expected.to eq('HelloWorld123')
+        end
+      end
+  
+      context 'and a letter in which the ciphering would precede a' do
+        let(:word) { 'KboorZruog456' }
+  
+        it 'should return the unciphered word correctly' do
+          is_expected.to eq('HylloWorld123')
+        end
+      end
+
+      context 'and a number in which the ciphering would precede 0' do
+        let(:word) { 'KhoorZruog451' }
+  
+        it 'should return the unciphered word correctly' do
+          is_expected.to eq('HelloWorld128')
+        end
+      end
+
+      context 'and a word with any other character than letters and numbers' do
+        let(:word) { 'KhoorZruog450!' }
+  
+        it 'should ignore unpermitted characters and return the unciphered word correctly' do
+          is_expected.to eq('HelloWorld127!')
+        end
+      end
+    end
+  end
+end
--- strategies/fixed_shift_cipher.rb
+++ strategies/fixed_shift_cipher.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 # This class is an adaptation to Caesar Cipher, allowing ciphering a word with letters and numbers only. 
-class ShiftCipher
+class FixedShiftCipher
   class << self
     def cipher(word, shift_factor: 3)
-      #check for letters and numbers [\w\d]
       return word unless shift_factor > 0
 
       result = ''
@@ -14,7 +13,7 @@ class ShiftCipher
     end
 
     def uncipher(word, unshift_factor: 3)
-      return word unless shift_factor > 0
+      return word unless unshift_factor > 0
 
       result = ''
       word.each_char { |char| result += unshift_char(char, unshift_factor) }
@@ -25,6 +24,7 @@ class ShiftCipher
     private
 
     def shift_char(char, shift_factor)
+      return char if char == '_' || /\W/.match?(char)
       return ('a'.ord + shift_factor - 1).chr if char == 'z'
       return ('A'.ord + shift_factor - 1).chr if char == 'Z'
       return ('0'.ord + shift_factor - 1).chr if char == '9'
@@ -33,9 +33,10 @@ class ShiftCipher
     end
 
     def unshift_char(char, unshift_factor)
-      return ('z'.ord - unshift_factor - 1).chr if char == 'a'
-      return ('Z'.ord - unshift_factor - 1).chr if char == 'A'
-      return ('9'.ord - unshift_factor - 1).chr if char == '0'
+      return char if char == '_' || /\W/.match?(char)
+      return ('z'.ord - unshift_factor + 1).chr if char == 'a'
+      return ('Z'.ord - unshift_factor + 1).chr if char == 'A'
+      return ('9'.ord - unshift_factor + 1).chr if char == '0'
 
       (char.ord - unshift_factor).chr
     end
