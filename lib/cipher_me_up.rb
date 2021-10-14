0a7ac6ce7e1ae5e29f994e3c69b074e7518a1a90
Kelly Bhering <kellybhering@hotmail.com>
2021-10-14 08:38:55 -0300

    Adjusts the file structure of the project

--- lib/cipher_me_up.rb
+++ lib/cipher_me_up.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'byebug'
-
-require 'letters_and_numbers_shifter'
-require 'string_helper'
-Dir[File.join('./ciphers/*.rb')].each { |file| require file }
+require 'cipher_me_up/letters_and_numbers_shifter'
+require 'cipher_me_up/string_helper'
+Dir[File.join('./lib/cipher_me_up/ciphers/*.rb')].each { |file| require file }
